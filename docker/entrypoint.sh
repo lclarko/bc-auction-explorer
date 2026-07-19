e06809f60f966a10b8a0f7be3e68ca89a6a25b10
Lane Clark <52838144+lclarko@users.noreply.github.com>
2026-07-19 03:50:49 -0700

    Stage secrets before dropping directory access

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,15 +13,14 @@ fi
 
 mkdir -p "$runtime_secret_directory"
 chmod 700 "$runtime_secret_directory"
-chown bcauction:bcauction "$runtime_secret_directory"
 
 if [ -n "${BC_AUCTION_DATABASE_PASSWORD_FILE:-}" ]; then
     umask 077
     cat "$BC_AUCTION_DATABASE_PASSWORD_FILE" > "$runtime_password_file"
     chown bcauction:bcauction "$runtime_password_file"
     chmod 600 "$runtime_password_file"
-    exec env BC_AUCTION_DATABASE_PASSWORD_FILE="$runtime_password_file" \
-        setpriv --reuid=bcauction --regid=bcauction --init-groups -- "$@"
+    export BC_AUCTION_DATABASE_PASSWORD_FILE="$runtime_password_file"
 fi
 
+chown bcauction:bcauction "$runtime_secret_directory"
 exec setpriv --reuid=bcauction --regid=bcauction --init-groups -- "$@"
