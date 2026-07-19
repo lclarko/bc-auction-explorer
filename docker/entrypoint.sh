eb667b6f7be362dc9972b06d158ddbb8bf7795c1
Lane Clark <52838144+lclarko@users.noreply.github.com>
2026-07-19 03:19:14 -0700

    Make the application entrypoint idempotent

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,16 +2,20 @@
 set -eu
 
 runtime_secret_directory=/tmp/bc-auction-secrets
-mkdir -p "$runtime_secret_directory"
-chmod 700 "$runtime_secret_directory"
-chown bcauction:bcauction "$runtime_secret_directory"
+if [ "$(id -u)" -eq 0 ]; then
+    mkdir -p "$runtime_secret_directory"
+    chmod 700 "$runtime_secret_directory"
+    chown bcauction:bcauction "$runtime_secret_directory"
 
-if [ -n "${BC_AUCTION_DATABASE_PASSWORD_FILE:-}" ]; then
-    runtime_password_file="$runtime_secret_directory/database_password"
-    cp "$BC_AUCTION_DATABASE_PASSWORD_FILE" "$runtime_password_file"
-    chown bcauction:bcauction "$runtime_password_file"
-    chmod 600 "$runtime_password_file"
-    export BC_AUCTION_DATABASE_PASSWORD_FILE="$runtime_password_file"
+    if [ -n "${BC_AUCTION_DATABASE_PASSWORD_FILE:-}" ]; then
+        runtime_password_file="$runtime_secret_directory/database_password"
+        cp "$BC_AUCTION_DATABASE_PASSWORD_FILE" "$runtime_password_file"
+        chown bcauction:bcauction "$runtime_password_file"
+        chmod 600 "$runtime_password_file"
+        export BC_AUCTION_DATABASE_PASSWORD_FILE="$runtime_password_file"
+    fi
+
+    exec setpriv --reuid=bcauction --regid=bcauction --init-groups "$@"
 fi
 
-exec setpriv --reuid=bcauction --regid=bcauction --init-groups "$@"
+exec "$@"
